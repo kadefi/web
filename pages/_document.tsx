051a07e77ff5241fc7c088416be984a4f024ee21
Straterra97 | Kadefi <kadefi.money@gmail.com>
2022-06-21 14:33:37 +0800

    refactor: move favicon to public since static is demised

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,7 +11,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="/static/favicon.ico" />
+          <link rel="shortcut icon" href="/public/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link
             rel="preconnect"
