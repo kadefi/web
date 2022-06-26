36d629c9761e0c8edc6f074c2a3a08e768cbab00
Kadonut <kadefi.money@gmail.com>
2022-06-26 14:41:19 +0800

    Fix variant mapping issue for Typography

--- src/theme.ts
+++ src/theme.ts
@@ -18,6 +18,23 @@ let theme = createTheme({
       main: red.A400,
     },
   },
+  components: {
+    MuiTypography: {
+      defaultProps: {
+        variantMapping: {
+          h1: "h1",
+          h2: "h2",
+          h3: "h3",
+          h4: "h4",
+          h5: "h5",
+          h6: "h6",
+          body1: "div",
+          body2: "span",
+        },
+        variant: "body1",
+      },
+    },
+  },
 });
 
 theme = responsiveFontSizes(theme);
