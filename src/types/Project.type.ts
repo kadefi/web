a02a85fe7897f70c57044694a1751a7fe0e5c1b8
Huiyeon Kim <huiyeon.kim.1228@gmail.com>
2022-06-28 00:15:51 +0800

    refactor: making adding new project or token slightly easier

--- src/types/Project.type.ts
+++ src/types/Project.type.ts
@@ -1,3 +1,13 @@
+/*
+ * NOTE:
+ * When adding new PROJECT_KEY or TOKEN_TICKER
+ * please make sure there is a corresponding image under
+ *
+ * "/public/assets/projects" or "/public/assets/tokens"
+ *
+ * based on what you are adding.
+ */
+
 export enum PROJECT_KEY {
   KD_SWAP = "kdswap",
   BABENA = "babena",
--- src/utils/Logo.util.tsx
+++ src/utils/Logo.util.tsx
@@ -1,28 +1,10 @@
 import PngLogo from "../components/commons/PngLogo";
 import { PROJECT_KEY, TOKEN_TICKER } from "../types/Project.type";
 
-const PROJECT_LOGO_MAP = {
-  [PROJECT_KEY.BABENA]: "babena.png",
-  [PROJECT_KEY.KADDEX]: "kaddex.png",
-  [PROJECT_KEY.KD_LAUNCH]: "kdlaunch.png",
-  [PROJECT_KEY.KD_SWAP]: "kdswap.png",
-  [PROJECT_KEY.ANEDAK]: "anedak.png",
-};
-
 export const getProjectLogo = (module: PROJECT_KEY) => {
-  return <PngLogo src={`/assets/projects/${PROJECT_LOGO_MAP[module]}`} size={1.75} />;
-};
-
-const TOKEN_LOGO_MAP = {
-  [TOKEN_TICKER.BABE]: "BABE.png",
-  [TOKEN_TICKER.ADK]: "ADK.png",
-  [TOKEN_TICKER.FLUX]: "FLUX.png",
-  [TOKEN_TICKER.HYPE]: "HYPE.png",
-  [TOKEN_TICKER.KDA]: "KDA.png",
-  [TOKEN_TICKER.KDL]: "KDL.png",
-  [TOKEN_TICKER.KDS]: "KDS.png",
+  return <PngLogo src={`/assets/projects/${module}.png`} size={1.75} />;
 };
 
 export const getTokenLogo = (token: TOKEN_TICKER) => {
-  return <PngLogo src={`/assets/tokens/${TOKEN_LOGO_MAP[token]}`} isCircular size={1.5} />;
+  return <PngLogo src={`/assets/tokens/${token}.png`} isCircular size={1.5} />;
 };
