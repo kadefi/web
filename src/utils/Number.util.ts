7bb1098ad242509679cfaf451e3d1b549cb9ba5e
Kadonut <kadefi.money@gmail.com>
2022-08-07 23:28:22 +0800

    feat: Show price lower than min value

--- src/utils/Number.util.ts
+++ src/utils/Number.util.ts
@@ -2,6 +2,12 @@ import round from "lodash/round";
 import numeral from "numeral";
 
 export const roundToDecimalStr = (num: number, decimalDigits: number) => {
+  const minValue = Number(Math.pow(10, -decimalDigits).toPrecision(1));
+
+  if (num < minValue) {
+    return `<${minValue}`;
+  }
+
   let optionalDigits = "";
 
   for (let i = 0; i < decimalDigits - 2; i++) {
